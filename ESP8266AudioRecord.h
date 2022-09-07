36cf31b303ef43658bb77d7b0e61bc96763dc2f2
SCratORS <scrators@mail.ru>
2022-09-07 15:04:44 +0300

    Update ESP8266AudioRecord.h

--- ESP8266AudioRecord.h
+++ ESP8266AudioRecord.h
@@ -35,7 +35,7 @@ private:
 	void timer_stop();
 	ESP8266AudioRecord();
 	uint8_t buffer[2][AUDIO_BUFFER_MAX];
-  	uint8_t header[44] = {'R','I','F','F',0,0,0,0,'W','A','V','E','f','m','t',' ',0x10,0,0,0,1,0,1,0,0x40,0x1F,0,0,0x40,0x1F,0,0,1,0,8,0,'d','a','t','a',0,0,0,0};/
+  	uint8_t header[44] = {'R','I','F','F',0,0,0,0,'W','A','V','E','f','m','t',' ',0x10,0,0,0,1,0,1,0,0x40,0x1F,0,0,0x40,0x1F,0,0,1,0,8,0,'d','a','t','a',0,0,0,0};
 //  	WiFiUDP UDP;
   	bool recording;
   	volatile bool buffer_ready;
