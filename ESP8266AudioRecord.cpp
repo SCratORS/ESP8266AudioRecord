4016e97e134ed0723be350b78f503f05b3d1cbfd
SCratORS <scrators@mail.ru>
2022-09-07 14:34:37 +0300

    Update ESP8266AudioRecord.cpp

--- ESP8266AudioRecord.cpp
+++ ESP8266AudioRecord.cpp
@@ -1,52 +1,45 @@
 #include "ESP8266AudioRecord.h"
-#include <LittleFS.h>
 
 void ESP8266AudioRecord::init(const uint32_t timer_delay)
 {
-    _TIMER_DELAY=timer_delay;
+	_TIMER_DELAY=timer_delay;
 	timer1_isr_init();
 }
-ESP8266AudioRecord::ESP8266AudioRecord() {}
+ESP8266AudioRecord::ESP8266AudioRecord() {
+	UDP.begin(UDP_PORT);
+}
 
 ESP8266AudioRecord* ESP8266AudioRecord::getInstance() {
-    static ESP8266AudioRecord instance; 
-    return &instance;
+	static ESP8266AudioRecord instance; 
+	return &instance;
 }
 bool ESP8266AudioRecord::RecordStart(const String &fname) {
 	if (recording) return false;
-	file = LittleFS.open(fname, "w");
-	if (!file) return false;
 	recording = true;
 	active_buffer = 0;
 	buffer_pointer = 0;
 	buffer_ready = false;
-	file.seek(0);
-	file.write((const byte *)header, sizeof(header));
-    timer1_attachInterrupt((void(*)()) &ESP8266AudioRecord::timer1_handler);
-    timer_start();
+	timer1_attachInterrupt((void(*)()) &ESP8266AudioRecord::timer1_handler);
+	timer_start();
 	return true;
 }
 void ESP8266AudioRecord::timer_start() {
-    timer1_write(_TIMER_DELAY);
-    timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
+	timer1_write(_TIMER_DELAY);
+	timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
 }
 void ESP8266AudioRecord::timer_stop() {
-    timer1_disable();
+	timer1_disable();
 }
 bool ESP8266AudioRecord::RecordStop() {
 	if (!recording) return false;
-    timer_stop();
+	timer_stop();
   	timer1_detachInterrupt();
   	recording = false;
-  	if (buffer_pointer > 0 && data_size < FILE_LENGTH) {
-    	data_size += file.write((const uint8_t *)buffer[(uint8_t)active_buffer], buffer_pointer);
+  	if (buffer_pointer > 0) {
+		UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+		UDP.write((const uint8_t *)buffer[(uint8_t)active_buffer], buffer_pointer);
+		UDP.endPacket();
   	}
-  	uint32_t file_size = data_size + 36;
-  	memcpy(header + 4, &file_size, 4);
- 		memcpy(header + 40, &data_size, 4);
-  	file.seek(0);
-  	file.write((const byte *)header, sizeof(header));
-  	file.close();
   	return true;
 }
 
@@ -57,14 +50,13 @@ bool ESP8266AudioRecord::Recording() {
 void ESP8266AudioRecord::RecordHandle() {
 	if (!recording) return;
 	if (buffer_ready) {
-    	buffer_ready = false;
-        uint8_t selbuf = !active_buffer;
-        timer_stop();
-    	data_size += file.write((const uint8_t *)buffer[selbuf], AUDIO_BUFFER_MAX);
-        //data_size += AUDIO_BUFFER_MAX;
-        Serial.printf("%d ",data_size);
-        if (data_size > FILE_LENGTH) RecordStop();
-        timer_start();
+    		buffer_ready = false;
+       		uint8_t selbuf = !active_buffer;
+        	timer_stop();
+		UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+		UDP.write((const uint8_t *)buffer[selbuf], AUDIO_BUFFER_MAX);
+		UDP.endPacket();
+        	timer_start();
   	}
 }
 void IRAM_ATTR ESP8266AudioRecord::timer1_handler() {
